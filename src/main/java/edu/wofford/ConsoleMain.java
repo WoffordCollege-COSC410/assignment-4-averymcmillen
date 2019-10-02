de5c8577097abd35ac8d28786d843fb64549a986
Avery McMillen <averymcmillen@gmail.com>
2019-10-02 14:54:19 -0400

    Commit - passing tests: 4/11 - made changes in win / tie / none cases

--- src/main/java/edu/wofford/ConsoleMain.java
+++ src/main/java/edu/wofford/ConsoleMain.java
@@ -6,6 +6,16 @@ public class ConsoleMain {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
+		//we need to do something there]
+		//in - console version of the game
+		//	 | |
+		//	-----
+		//	 | |
+		//	-----
+		//	 | |
+		//with this thing
+		//
+		//
 
 	}
 }
--- src/main/java/edu/wofford/TicTacToeModel.java
+++ src/main/java/edu/wofford/TicTacToeModel.java
@@ -54,15 +54,9 @@ public class TicTacToeModel {
                 if (board[0][i] == Mark.XMARK) {
                     return Result.XWIN;
                 }
-                else if (board[i][0] == Mark.OMARK) {
+                else {
                     return Result.OWIN;
                 }
-            }
-            else if (board[i][i] != Mark.EMPTY) {
-                return Result.TIE;
-            }
-            else {
-                return Result.NONE;
             }
         }
         if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
@@ -82,15 +76,80 @@ public class TicTacToeModel {
             }
         }
         else{
-            //if (board[][]);
-            //return tie or none
+            for (int row = 0; row < board.length; row++) {
+                for (int col = 0; col < board.length; col++) {
+                    if (board[row][col] == Mark.EMPTY) {
+                        return Result.NONE;
+                    }
+                }
+            }
+
+            return Result.TIE;
         }
-        return Result.NONE;
     }
 
     public String toString() {
-        //what is this for??
-        return "";
+        //if board[][] is x add "x" or "o" or " " + "|" etc
+        String boardString = " ";
+
+        for (int row = 0; row < 1; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == Mark.XMARK && board[row][col] != board[row][2]) {
+                    boardString = "x" + "|";
+                } else if (board[row][col] == Mark.XMARK && board[row][col] == board[row][2]) {
+                    boardString = "x" + "\n";
+                } else if (board[row][col] == Mark.OMARK && board[row][col] != board[row][2]) {
+                    boardString = "o" + "|";
+                } else if (board[row][col] == Mark.OMARK && board[row][col] == board[row][2]) {
+                    boardString = "o" + "\n";
+                } else if (board[row][col] == Mark.EMPTY && board[row][col] != board[row][2]) {
+                    boardString = " " + "|";
+                } else {
+                    boardString = " " + "\n";
+                }
+            }
+        }
+
+        boardString = boardString + "-----";
+
+        for (int row = 1; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == Mark.XMARK && board[row][col] != board[row][2]) {
+                    boardString = "x" + "|";
+                } else if (board[row][col] == Mark.XMARK && board[row][col] == board[row][2]) {
+                    boardString = "x" + "\n";
+                } else if (board[row][col] == Mark.OMARK && board[row][col] != board[row][2]) {
+                    boardString = "o" + "|";
+                } else if (board[row][col] == Mark.OMARK && board[row][col] == board[row][2]) {
+                    boardString = "o" + "\n";
+                } else if (board[row][col] == Mark.EMPTY && board[row][col] != board[row][2]) {
+                    boardString = " " + "|";
+                } else {
+                    boardString = " " + "\n";
+                }
+            }
+        }
+
+        boardString = boardString + "-----";
+
+        for (int row = 2; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == Mark.XMARK && board[row][col] != board[row][2]) {
+                    boardString = "x" + "|";
+                } else if (board[row][col] == Mark.XMARK && board[row][col] == board[row][2]) {
+                    boardString = "x" + "\n";
+                } else if (board[row][col] == Mark.OMARK && board[row][col] != board[row][2]) {
+                    boardString = "o" + "|";
+                } else if (board[row][col] == Mark.OMARK && board[row][col] == board[row][2]) {
+                    boardString = "o" + "\n";
+                } else if (board[row][col] == Mark.EMPTY && board[row][col] != board[row][2]) {
+                    boardString = " " + "|";
+                } else {
+                    boardString = " " + "\n";
+                }
+            }
+        }
+        return boardString;
     }
 
 }
