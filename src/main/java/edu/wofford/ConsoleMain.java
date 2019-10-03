5d9331dcfd40636abd3de2fc64d962fd002c7efb
Avery McMillen <averymcmillen@gmail.com>
2019-10-03 18:45:53 -0400

    Everything done

--- src/main/java/edu/wofford/ConsoleMain.java
+++ src/main/java/edu/wofford/ConsoleMain.java
@@ -6,6 +6,8 @@ public class ConsoleMain {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
+
+		TicTacToeModel model = new TicTacToeModel();
 		//we need to do something there]
 		//in - console version of the game
 		//	 | |
@@ -16,6 +18,30 @@ public class ConsoleMain {
 		//with this thing
 		//
 		//
+        while (model.getResult() == TicTacToeModel.Result.NONE){
+            System.out.println(model.toString());
+            String currentLine = keyboard.nextLine();
+
+            int row = 0, col = 0;
+            if (currentLine.length() == 3){
+                row = Character.getNumericValue(currentLine.charAt(0));
+                col = Character.getNumericValue(currentLine.charAt(2));
+            }
+
+            model.setMarkAt(row, col);
+        }
+
+        switch (model.getResult()){
+            case XWIN:
+                System.out.println("X wins");
+                break;
+            case OWIN:
+                System.out.println("O wins");
+                break;
+            case TIE:
+                System.out.println("Tie");
+                break;
+        }
 
 	}
 }
